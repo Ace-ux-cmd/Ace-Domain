135230db8972c9aa25e61a7e277b0dee4893c7ed
Ace <uchihaace47@gmail.com>
2025-04-14 10:08:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+const textBase = "New content coming soon";
+  const element = document.getElementById("loading");
+  let dots = 1;
 
+  setInterval(() => {
+    element.textContent = textBase + ".".repeat(dots);
+    dots = dots < 3 ? dots + 1 : 1;
+  }, 1000);
