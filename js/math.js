fe6a1259177845439065890ab3a736f9fa796a36
Ace <uchihaace47@gmail.com>
2025-04-18 18:18:01 +0100

    Update math.js

--- js/math.js
+++ js/math.js
@@ -15,7 +15,8 @@ const multiplication= document.getElementById('multi');
 const division=document.getElementById('div');
 const power=document.getElementById('exp');
 const remainder= document.getElementById('mod');
-const button= document.getElementById('button')
+const button= document.getElementById('button');
+ const text= document.getElementById('Value');
 
 //For addition
 button.onclick=function(){
@@ -39,7 +40,7 @@ const  expo= Number (document.getElementById('Value1').value)** Number(document.
  const  remain= Number (document.getElementById('Value1').value)% Number(document.getElementById('Value2').value);
  
  //Values
- 
+ text.textContent = 'Values';
   addition.textContent= `${x} + ${y} = ${add}`;
   subtraction.textContent= `${x} - ${y} = ${minus}`;
   multiplication.textContent= `${x} × ${y} = ${multiply}`;
